69145a4d95433e7b04ec4541dc1f361643611599
Ayush kumar mishra <ayushkumarmishra107@gmail.com>
2026-07-11 22:47:06 +0530

    Create Rotate array dsa complete

---
+++ Rotate array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+    
+    const n = matrix.length;
+
+    // Transpose
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+        }
+    }
+
+    // Reverse each row
+    for (let row of matrix) {
+        row.reverse();
+    }
+
+    return matrix;
+
+};
