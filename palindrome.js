77585c6cf3d36b33eeb2ce5e5c4ba0dd747e26c0
Ayush kumar mishra <ayushkumarmishra107@gmail.com>
2026-07-12 22:27:22 +0530

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -11,3 +11,18 @@ if(str===rev){
     else{
         console.log("no")
     }
+
+/*----For Number-------*/
+
+let num = 83838
+let str num.toString()
+   let rev=""
+   for(let i = str.length -1;i>=0;i--){
+       rev += str[i]
+   }
+if(str===rev){
+    console.log("yes")
+}
+    else{
+        console.log("no")
+    }
